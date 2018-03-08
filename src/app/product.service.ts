7592c5981e46267b89f1e5cfab9204636dc6b4fa
Akos Kerekgyarto <akos.kerekgyarto@avanade.com>
2018-03-08 16:37:16 +0100

    IMP: Introduce Types

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http'
 import 'rxjs/add/operator/map';
@@ -7,9 +9,9 @@ export class ProductService {
   private _albumUrl: string = "../assets/album.json";
   constructor(private _http: Http ) { }
 
-  getAlbum(id: number){
+  getAlbum(id: number) : Observable<Album>{
     return this._http.get(this._albumUrl).map((response) => 
-      response.json()
+      <Album>response.json()
     );
   }
 }
