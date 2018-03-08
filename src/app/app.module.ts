305840cfabb604e95000eb7ec40d5e0eef630a7a
Akos Kerekgyarto <akos.kerekgyarto@avanade.com>
2018-03-08 17:59:14 +0100

    IMP: Route order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,10 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 import { appendFile } from 'fs';
 
-const appRoutes: Routes =[
-  {path: '', redirectTo:'products', pathMatch:'full'},
+const appRoutes: Routes =[  
   {path: 'products', component: ProductListComponent},
   {path: 'product/:id', component: ProductPageComponent},
+  {path: '', redirectTo:'products', pathMatch:'full'}
 ];
 
 
